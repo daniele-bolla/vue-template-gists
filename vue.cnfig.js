44f1bef531c091bc6d5626afc27699e496f492c9
bolladaniele <bolladaniele@gmail.com>
2020-11-23 09:04:06 +0000

    change home

--- vue.cnfig.js
+++ vue.cnfig.js
@@ -1,11 +1,5 @@
 module.exports = {
-  css: {
-    loaderOptions: {
-      sass: {
-        additionalData: `@import "@/assets/variables.scss";`
-      }
-    }
-  },
+
   publicPath: "/vue-template-gists/"
 
 };
